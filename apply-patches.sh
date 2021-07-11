35cc7a2396baa63d6b3e7cc7ad4eed0e0fbccfa6
Jayden Chan <jaydencn7@gmail.com>
2021-07-11 14:46:44 -0700

    JC: newterm patch

--- apply-patches.sh
+++ apply-patches.sh
@@ -10,3 +10,5 @@ nvim st.h
 patch --merge < patches/st-anysize-0.8.4.diff
 patch --merge < patches/st-blinking_cursor-20200531-a2a7044.diff
 patch --merge < patches/st-vertcenter-20180320-6ac8c8a.diff
+patch --merge < patches/st-newterm-0.8.2.diff
+nvim config.def.h
--- config.h
+++ config.h
@@ -220,6 +220,7 @@ static Shortcut shortcuts[] = {
 	{ ShiftMask,            XK_Insert,      selpaste,       {.i =  0} },
 	{ TERMMOD,              XK_Num_Lock,    numlock,        {.i =  0} },
 	{ MODKEY,               XK_k,           copyurl,        {.i =  0} },
+	{ TERMMOD,              XK_Return,      newterm,        {.i =  0} },
 };
 
 /*
