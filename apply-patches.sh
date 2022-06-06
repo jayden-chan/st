360e9d6dcd8665764fbbc9ed77fbbbc726a98e9f
Jayden Chan <jaydencn7@gmail.com>
2022-06-06 02:02:52 -0700

    JC: Xresources

--- apply-patches.sh
+++ apply-patches.sh
@@ -2,15 +2,16 @@
 
 set -x
 
-patch --merge < patches/st-boxdraw_v2-0.8.3.diff
-patch --merge < patches/st-ligatures-boxdraw-20200430-0.8.3.diff
-patch --merge < patches/st-copyurl-0.8.4.diff
-patch --merge < patches/st-osc10-20210106-4ef0cbd.diff
+patch --merge < ./patches/st-boxdraw_v2-0.8.3.diff
+patch --merge < ./patches/st-ligatures-boxdraw-20200430-0.8.3.diff
+patch --merge < ./patches/st-copyurl-0.8.4.diff
+patch --merge < ./patches/st-osc10-20210106-4ef0cbd.diff
 nvim st.h
-patch --merge < patches/st-anysize-0.8.4.diff
-patch --merge < patches/st-blinking_cursor-20200531-a2a7044.diff
-patch --merge < patches/st-vertcenter-20180320-6ac8c8a.diff
-patch --merge < patches/st-newterm-0.8.2.diff
+patch --merge < ./patches/st-anysize-0.8.4.diff
+patch --merge < ./patches/st-blinking_cursor-20200531-a2a7044.diff
+patch --merge < ./patches/st-vertcenter-20180320-6ac8c8a.diff
+patch --merge < ./patches/st-newterm-0.8.2.diff
 nvim config.def.h
-patch --merge < patches/st-externalpipe-0.8.4.diff
+patch --merge < ./patches/st-externalpipe-0.8.4.diff
+patch --merge < ./patches/st-xresources-signal-reloading-20220407-ef05519.patch
 nvim st.h
